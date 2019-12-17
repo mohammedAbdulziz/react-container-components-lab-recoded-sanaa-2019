e7982f3f5c0165336297b694dc6ef2357c46c808
Mohammed  Abdulaziz <mohammedazizbuss@gmail.com>
2019-12-17 02:51:03 +0000

    Automatically backed up by Learn

--- src/components/LatestMovieReviewsContainer.js
+++ src/components/LatestMovieReviewsContainer.js
@@ -2,8 +2,31 @@ import React, { Component } from 'react';
 import 'isomorphic-fetch';
 import MovieReviews from './MovieReviews'
 
-const NYT_API_KEY = 'dGpQ5OmGP2SgfvZimlpCUoF4iOag9qzZ';
+const NYT_API_KEY = '3VGYfs6cW0PSV2oABBOB5qUDBZzGCY19';
 const URL = 'https://api.nytimes.com/svc/movies/v2/reviews/all.json?'
             + `api-key=${NYT_API_KEY}`;
 
 // Code LatestMovieReviewsContainer Here
+export class LatestMovieReviewsContainer extends Component {
+    constructor(props) {
+        super(props)
+    
+        this.state = {
+            reviews:[]
+        }
+    }
+    
+    componentDidMount(){
+        fetch(URL).then(json=>json.json()).then(json=>this.setState({reviews:json.results}))
+    }
+    render() {
+        return (
+            <div className="latest-movie-reviews">
+                <MovieReviews reviews={this.state.reviews} />
+
+            </div>
+        )
+    }
+}
+
+export default LatestMovieReviewsContainer
